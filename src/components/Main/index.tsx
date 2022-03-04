91a857330aac708c067797a4ff63ba7b21600674
nicolasteofilo <nicolasteofilodecastro@gmail.com>
2022-03-03 22:20:55 -0300

    feat: create basic map component

--- src/components/Main/index.tsx
+++
@@ -1,5 +0,0 @@
-import * as S from './styles'
-
-const Main = () => <h1>Test</h1>
-
-export { Main }
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -10,6 +10,12 @@ export default function App({ Component, pageProps }: AppProps) {
         <title>My trips</title>
         <link rel="shortcut icon" href="favicon.ico" type="image/x-icon" />
         <link rel="manifest" href="/manifest.json" />
+        <link
+          rel="stylesheet"
+          href="https://unpkg.com/leaflet@1.7.1/dist/leaflet.css"
+          integrity="sha512-xodZBNTC5n17Xt2atTPuE1HxjVMSvLVW9ocqUKLsCC5CXdbqCmblAshOMAS6/keqq/sMZMZ19scR4PsZChSR7A=="
+          crossOrigin=""
+        />
         <meta name="theme-color" content="#06092b" />
         <meta
           name="description"
